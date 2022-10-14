eb751dd2e6d8e9eb974a72e23bc0e7accf61393c
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-14 13:49:31 -0700

    fourth task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
-#include <stdio.h>                                                                                                                                       
-                                                                                                                                                 
-/**                                                                                                                                                    
- * main-program entry point                                                                                                                              
- * Return:0 id no error, non zero if error                                                                                                                **/                                                                                                                                                     
-int main(void)                                                                                                                                         
+#include <stdio.h>
+
+/**
+ * main-programentry point.
+ * Return:0 no error, non zero if error.
+ **/
+int main(void)
 {
 	char i;
 	char j;
 
-	for (i = 'a'; i <== 'z'; i++)
+	for (i = 'a'; i < = 'z'; i++)
 	{
 		putchar(i);
 	}
-	for (j= 'A'; j <= 'Z'; j++)
+	for (j = 'A'; j < = 'Z'; j++)
 	{
 		putchar(j);
 	}
 	putchar('\n');
 	return (0);
-}	
+}

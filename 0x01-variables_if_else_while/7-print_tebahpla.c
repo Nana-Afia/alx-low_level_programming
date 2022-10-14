6deb8d4892baf3c860fadc71cdc831139b27b869
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-14 14:40:03 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-programentry point
+ * Return:0 if no errors, non zero if errors
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >='a'; i--)
+		putchar(i);
+	putchar('\n')
+		return (0);

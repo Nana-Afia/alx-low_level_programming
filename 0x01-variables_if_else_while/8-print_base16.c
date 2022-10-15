902ef0e86e1610bfcced55ebba9efbd7b9de2042
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-15 11:09:11 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 	int i;
-	char hexvalues[] = "0123456789abvdef";
+	char hexvalues[] = "0123456789abcdef";
 
-	for (i = 0; i < 16; i++)
+	for (i=0; i < 16; i++)
 	{
 		putchar(hexvalues[i]);
 	}
 	putchar('\n');
 	return (0);
-}
+}	

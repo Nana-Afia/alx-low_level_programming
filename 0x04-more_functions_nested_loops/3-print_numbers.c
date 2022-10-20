0344a7aeb2503f7dd616193ca3efe84d4165610a
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-20 15:51:05 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints from 0 to 9
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}

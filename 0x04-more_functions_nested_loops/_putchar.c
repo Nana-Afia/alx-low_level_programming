99ead278f1c79b8b139a389ef2e387359dc068ad
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-20 15:29:47 -0700

    Task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes te character c to stdout
+ * _putchar- writes te character c to stdout
  * @c: The character to print
- * Return: On success 1 else -1
+ * Return: On success
  */
 int _putchar(char c)
 {

f06280403b868b155820c7ed1413e1bbd7da71a6
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-27 04:08:24 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenate two strings
+ * @*dest: destination string
+ * @*src: source string
+ * Return: always return 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x06 more examples on pointers and arrays

4913c84f16ad0a25faf39e69c4874d6ed2b36877
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-27 05:48:53 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ *@a: array
+ * @n: number of elements of the array
+ * Return: rev
+ */
+void reversse_array(int *a, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+		n--;
+		b = x[a];
+		x[a] = a[n];
+		x[n] = b;
+	}
+}	

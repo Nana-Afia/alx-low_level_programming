1bdd9f7d572f8e2b362d3bd73f37fee4ad002024
Nana-Afia <naowusuachiaw@yahoo.com>
2022-10-31 11:37:54 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,15 +4,19 @@
  * @s: A pointer to a memory to be filled
  * @c: A character to fill the memory with
  * @n: The number of bytes to be filled
- * Return: a pointer to the filled memory area @s
+ * Return: a pointer to the filled memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned int num;
+	unsigned char *s;
+	unsigned char b;
+	unsigned int n;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	num = 0;
+	while num < n
+		s = &b
+		_putchar(b);	
 
-	return(memory);
+	return(*s)
 }

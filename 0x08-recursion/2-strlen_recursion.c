6eef9a31a0891a3f34ed87417df1497aeadd0eba
Nana-Afia <naowusuachiaw@yahoo.com>
2022-11-02 12:35:57 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+##include "main.h"
+
+/**
+ * _strleng_recursion - function that prints the length of a string
+ * @s: string to be printed
+ * Return: length of the string
+ */
+int _strleng_recursion(char *s)
+{
+	int n;
+
+
+	if (*s == '\0')
+		return (0);
+	s++;
+	return(1 + _strlen_recursion(s));
+}

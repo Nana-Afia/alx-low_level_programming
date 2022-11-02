43512ddb6f6998f88efd00fb948ab1bb0b6c37c0
Nana-Afia <naowusuachiaw@yahoo.com>
2022-11-02 13:26:09 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns natural square of a num
+ * @n: paramter to squared
+ * Return: square of the number
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}

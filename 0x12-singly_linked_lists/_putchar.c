60f72f64f348a9091b5ede5b8af7bda9318fe603
Nana-Afia <naowusuachiaw@yahoo.com>
2022-11-24 08:51:51 -0800

    help

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,1 +1,13 @@
 #include <unistd.h>
+#include "lists.h"
+
+/**
+ * _putchar - prints char c to stdout
+ * @c: char to be printed
+ *
+ * Return: On success 1, else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

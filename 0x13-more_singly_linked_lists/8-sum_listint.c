b98937d151f866c619bf6b16f4c55ed55d5d6174
Nana-Afia <naowusuachiaw@yahoo.com>
2022-11-29 06:13:48 -0800

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - Calculates the sum of all the 
+ * sum_listint - Calculates the sum of all the
  * data (n) of the listint_t list.
  * @head: A pointer to the head of the listint_t list.
  *

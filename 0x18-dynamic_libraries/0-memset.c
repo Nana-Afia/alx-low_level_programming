af9d5dd4d3e3268215d1bc1a19d3b1375aef701f
Nana-Afia <naowusuachiaw@yahoo.com>
2023-01-16 02:13:04 -0800

    all

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * memset - fills the first n bytes of the memory area pointed to by @s with constant byte
+ * @s: A pointer to a memory to be filled
+ * @c: A character to fill the memory with
+ * @n: The number of bytes to be filled
+ * Return: a pointer to the filled memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int num;
+
+	for (num = 0; num < n; num++)
+	{
+		*(s + num) = b;
+	}
+
+	return(s);
+}
